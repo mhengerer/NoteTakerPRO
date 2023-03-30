c8c39376e410457a491e8cf0d295dc9372f10106
mhengerer <112892304+mhengerer@users.noreply.github.com>
2023-03-30 16:50:52 -0400

    fix small bug 2

--- routes/notes.js
+++ routes/notes.js
@@ -5,18 +5,18 @@ const { v4: uuidv4 } = require("uuid");
 const dataBase = require("../db/db.json");
 
 notes.get("/notes", (req, res) => {
-    res.json(dataBase)
-})
+  res.json(dataBase);
+});
 
 notes.post("/notes", (req, res) => {
-    let addNote = {
-        title: req.body.title,
-        body: req.body.text,
-        notes_id: uuidv4(),
-    }
-    dataBase.push(addNote)
-    fs.writeFileSync("../db/db.json", JSON.stringify(dataBase))
-    res.json(dataBase)
-})
+  let addNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4(),
+  };
+  dataBase.push(addNote);
+  fs.writeFileSync("../db/db.json", JSON.stringify(dataBase));
+  res.json(dataBase);
+});
 
 module.exports = notes;
